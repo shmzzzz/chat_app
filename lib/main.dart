5b0defb97ff270c77c1235eaca9c9eb78d7c66e0
shmzzzz <slt0623@gmail.com>
2023-10-26 08:41:43 +0900

    💄 🚸 Firebase がトークンを取得している間に表示するローディング(Splash)画面の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:chat_app/screens/chat.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
+import 'firebase_options.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
 import 'package:chat_app/screens/auth.dart';
 
 void main() async {
@@ -29,6 +30,10 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
           if (snapshot.hasData) {
             return const ChatScreen();
           }
